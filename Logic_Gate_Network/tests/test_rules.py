2b0901aed0fc9b3a75ca7bd9b5f10585a34bb1cc
Yuh JooKyung <95693039+JooKyung-Yuh@users.noreply.github.com>
2025-11-13 23:22:36 +0900

    [feat] test_rules.py에 rule checker 추가

--- Logic_Gate_Network/tests/test_rules.py
+++ Logic_Gate_Network/tests/test_rules.py
@@ -1,5 +1,6 @@
 import pytest # Python 테스트 프레임워크
 from ..rules import Rule1_NoConsecutive1s
+from ..rules import RuleChecker
 
 def test_rule1_valid_cases():
   rule1 = Rule1_NoConsecutive1s()
@@ -29,3 +30,29 @@ def test_rule1_metadata():
   assert rule1.get_dimension() == 10
   assert rule1.get_name() == "No Consecutive 1s"
   assert rule1.get_description() == "Valid if no two adjacent positions are both 1"
+  
+def test_rulechecker_add_rule():
+  rulechecker = RuleChecker()
+  rule1 = Rule1_NoConsecutive1s()
+  rulechecker.add_rule(rule1)
+  
+  assert len(rulechecker.get_rules()) == 1
+  
+def test_rulechecker_validate_all_pass():
+  rulechecker = RuleChecker()
+  rule1 = Rule1_NoConsecutive1s()
+  
+  rulechecker.add_rule(rule1)
+  sequence = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+  
+  assert rulechecker.validate_all(sequence) == True
+  
+def test_rulechecker_validate_all_fail():
+  rulechecker = RuleChecker()
+  rule1 = Rule1_NoConsecutive1s()
+  
+  rulechecker.add_rule(rule1)
+  sequence = [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+  
+  assert rulechecker.validate_all(sequence) == False
+  
