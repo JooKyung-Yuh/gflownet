b75a7411015f8981bfa63c82616e8b5813db69b9
Yuh JooKyung <95693039+JooKyung-Yuh@users.noreply.github.com>
2025-11-18 02:43:02 +0900

    [feat] lgn export __init__.py 추가

--- Logic_Gate_Network/lgn/__init__.py
+++ Logic_Gate_Network/lgn/__init__.py
@@ -1,7 +1,14 @@
 from .gates import GateType, is_valid_arity, apply_gate
+from .network import Gate, LGNState
+from .evaluator import LGNEvaluator
+from .action_space import ActionSpace
 
 __all__ = [
   "GateType",
   "is_valid_arity",
   "apply_gate",
+  "Gate",
+  "LGNState",
+  "LGNEvaluator",
+  "ActionSpace",
 ]
